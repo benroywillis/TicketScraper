05c218e37aea3a51a0acbdf3c0c99b93cdd5479c
benroywillis <benroywillis@gmail.com>
2022-03-17 09:31:02 -0700

    New data and a new script to pull lots of events in one swoop for MMW

---
+++ Scripts/MMW.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+python3 StubHub.py -l https://www.stubhub.com/link-miami-rebels-miami-tickets-3-23-2022/event/105209955/ -p MMWOpeningParty
+python3 StubHub.py -l https://www.stubhub.com/drumcode-miami-tickets-3-24-2022/event/105197105/ -p DrumCodeMMW
+python3 StubHub.py -l https://www.stubhub.com/solid-grooves-miami-tickets-3-25-2022/event/105200976/ -p SolidGroovesMMW
+python3 StubHub.py -l https://www.stubhub.com/dixon-miami-tickets-3-26-2022/event/105201359/ -p DixonMMW
+python3 StubHub.py -l https://www.stubhub.com/link-miami-rebels-miami-tickets-3-27-2022/event/105226617/ -p MMWClosingParty
