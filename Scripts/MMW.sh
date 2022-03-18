47b63a9385012f9edca31967f22d9eb7734152d9
benroywillis <benroywillis@gmail.com>
2022-03-18 09:15:28 -0700

    Fixed bug with the top comment in the script

--- Scripts/MMW.sh
+++ Scripts/MMW.sh
@@ -1,5 +1,4 @@
-!/bin/bash
-
+#!/bin/bash
 python3 StubHub.py -l https://www.stubhub.com/link-miami-rebels-miami-tickets-3-23-2022/event/105209955/ -p MMWOpeningParty
 python3 StubHub.py -l https://www.stubhub.com/drumcode-miami-tickets-3-24-2022/event/105197105/ -p DrumCodeMMW
 python3 StubHub.py -l https://www.stubhub.com/solid-grooves-miami-tickets-3-25-2022/event/105200976/ -p SolidGroovesMMW
